7fc6703abf8b2fff20b1749d3db5efde659390c4
Movchan94 <tkd.movchan@gmail.com>
2020-04-08 14:45:40 +0300

    added build

---
+++ precache-manifest.6ca5e5cb6a0ab67c97a9fb33e16bcd4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1886602285c56982430f0d72190f465b",
+    "url": "/TestHeader/index.html"
+  },
+  {
+    "revision": "49d4a74b1a448614ea72",
+    "url": "/TestHeader/static/js/2.d1dbe0f8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TestHeader/static/js/2.d1dbe0f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f05be272b6d47363b3",
+    "url": "/TestHeader/static/js/main.e6bf1fa4.chunk.js"
+  },
+  {
+    "revision": "b18642c515b7dc102f56",
+    "url": "/TestHeader/static/js/runtime-main.9676a1c0.js"
+  }
+]);
